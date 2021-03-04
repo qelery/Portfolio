ae1439a3abfb5579fd9a1e9d0990d2959a732c08
Quincy Elery <quincyelery@gmail.com>
2021-03-04 11:40:07 -0600

    added left and right scrolls to smoothscrolls

--- scripts/smoothScroll.js
+++ scripts/smoothScroll.js
@@ -15,3 +15,46 @@ toTopButton.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+const leftTiles = document.querySelectorAll("article.tile.left");
+leftTiles.forEach(e => setLeftTileListeners(e));
+
+const rightTiles = document.querySelectorAll(".tile.right");
+rightTiles.forEach(e => setRightTileListeners(e));
+
+const lastTile = document.querySelector("article.tile.last");
+
+lastTile.addEventListener('mouseover', (event) => {
+    event.target.classList.remove("movesUp");
+    event.target.classList.add("movesDown");
+});
+
+lastTile.addEventListener('mouseout', (event) => {
+    event.target.classList.remove("movesDown");
+    event.target.classList.add("movesUp");
+});
+
+
+function setLeftTileListeners(element) {
+    element.addEventListener('mouseover', (event) => {
+        event.target.classList.remove("leftToHomePosition");
+        event.target.classList.add("leftMovesRight");
+    });
+
+    element.addEventListener('mouseout', (event) => {
+        event.target.classList.remove("leftMovesRight");
+        event.target.classList.add("leftToHomePosition");
+    });
+}
+
+function setRightTileListeners(element) {
+    element.addEventListener('mouseover', (event) => {
+        event.target.classList.remove("rightToHomePosition");
+        event.target.classList.add("rightMovesLeft");
+    })
+
+    element.addEventListener('mouseout', (event) => {
+        event.target.classList.remove("rightMovesLeft");
+        event.target.classList.add("rightToHomePosition");
+    })
+}
